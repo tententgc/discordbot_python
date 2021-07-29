135af46ae71106536fb5abdc13918db8088213ce
tententgc <nyman3527@gmail.com>
2021-07-29 14:29:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
 #866945048339546122  client 
-#ODY2OTQ1MDQ4MzM5NTQ2MTIy.YPZ7fg.5Dxvt_lWRKlifejqf1ktEBtIyuQ  token
 #8 permission
 import discord
+from discord import message
+from discord import channel
 from discord.client import Client
+from discord.ext import commands
+from datetime import date, datetime, timedelta
 
-client = discord.Client()
-#wrapper decurator
-@client.event
+#client = discord.Client()
+
+
+message_lastseen = datetime.now()
+message2_lastseen = datetime.now()
+
+bot = commands.Bot(command_prefix='tt',help_command=None)
+
+
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print(f"Logged in as {bot.user}")
 
-@client.event #async / await
+@bot.command()
+async def test(ctx, *,par):
+    await ctx.channel.send("you type{0}".format(par))
+
+@bot.command()
+async def help(ctx):
+    await ctx.channel.send("help, test")
+
+@bot.event #async / await
 async def on_message(message):
-    if message.content ==('555'or'5555'):
+    global message_lastseen,message2_lastseen
+    if message.content =='555':
         await message.channel.send("ขำหาพ่องมึงหรอ")
     elif message.content =="!user":
         await message.channel.send(str(message.author.name)+"  hello")
+    elif message.content == 'ชื่ออะไรหรอ' and  datetime.now()>= message_lastseen:
+        message_lastseen = datetime.now()+timedelta(seconds=3)
+        await message.channel.send("ฉันชื่อ "+str(bot.user.name))
+        #logging
+        print('{0} call name at {1} and use after {2}'.format(message.author.name,datetime.now(),message_lastseen))
+    elif message.content == 'ผมชื่ออะไรหรอ' and  datetime.now()>= message2_lastseen:
+        message2_lastseen = datetime.now()+timedelta(seconds=3)
+        await message.channel.send("ฉันชื่อ "+str(message.author.name))
     elif message.content =="!logout":
-        await client.logout()
+        await bot.logout()
+    await bot.process_commands(message)
 
-client.run('ODY2OTQ1MDQ4MzM5NTQ2MTIy.YPZ7fg.5Dxvt_lWRKlifejqf1ktEBtIyuQ')
+bot.run('ODY2OTQ1MDQ4MzM5NTQ2MTIy.YPZ7fg.5Dxvt_lWRKlifejqf1ktEBtIyuQ') #use fake token
 
+#ODY2OTQ1MDQ4MzM5NTQ2MTIy.YPZ7fg.T1x18JK7TWEznVwxyqIUNLJxgTQ    real token
